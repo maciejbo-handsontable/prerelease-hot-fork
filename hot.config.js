a41e86ff623f0083c75f75a4c5a6aed59c4e64c9
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-10-25 12:07:32 +0200

    14.2.2

--- hot.config.js
+++ hot.config.js
@@ -2,8 +2,8 @@ const moment = require('moment');
 
 module.exports = {
   HOT_FILENAME: 'handsontable',
-  HOT_VERSION: '12.1.3',
+  HOT_VERSION: '14.2.2',
   HOT_PACKAGE_NAME: 'handsontable',
   HOT_BUILD_DATE: moment().format('DD/MM/YYYY HH:mm:ss'),
-  HOT_RELEASE_DATE: '22/09/2022',
+  HOT_RELEASE_DATE: '20/12/2022',
 };
